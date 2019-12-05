9b3627efd49979873f749cbaae4415910bf0ecaf
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-05 01:10:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,7 +69,7 @@ def game_hash
         :assists => 1,
         :steals => 2,
         :blocks => 7,
-        :slam_dunks => 2
+        :slam_dunks => 2,
       },{
         :player_name => "Bismack Biyombo",
         :number => 0,
@@ -79,7 +79,7 @@ def game_hash
         :assists => 7,
         :steals => 22,
         :blocks => 15,
-        :slam_dunks => 10
+        :slam_dunks => 10,
       },{
         :player_name => "DeSagna Diop",
         :number => 2,
@@ -89,7 +89,7 @@ def game_hash
         :assists => 12,
         :steals => 4,
         :blocks => 5,
-        :slam_dunks => 5
+        :slam_dunks => 5,
       },{
         :player_name => "Ben Gordon",
         :number => 8,
@@ -99,7 +99,7 @@ def game_hash
         :assists => 2,
         :steals => 1,
         :blocks => 1
-        :slam_dunks => 0
+        :slam_dunks => 0,
       },{
         :player_name => "Kemba Walker",
         :number => 33,
@@ -109,7 +109,7 @@ def game_hash
         :assists => 12,
         :steals => 7,
         :blocks => 5,
-        :slam_dunks => 12
+        :slam_dunks => 12,
       }]
     }
   }
